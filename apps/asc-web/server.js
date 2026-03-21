29152d9ebc2634b1db644bdd58affb22b5e060c1
slamhan <slamhan1987@hotmail.com>
2026-03-21 14:17:03 +0800

    fix(server): enable private network access in CORS headers

--- apps/asc-web/server.js
+++ apps/asc-web/server.js
@@ -86,6 +86,7 @@ const server = http.createServer(async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Private-Network', 'true');
 
   if (req.method === 'OPTIONS') {
     res.writeHead(204);
